da91e2842883c522d6a795628a964a8e93609620
Abdelrhman Alaa <alaasabry@Abdelrhmans-MacBook-Pro.local>
2020-08-06 02:58:42 +0200

    first commit

---
+++ contracts/project.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+
+contract Ballot {
+    struct land{
+    string name;
+    string coordinator;
+    uint area;
+    string landmark;
+    }
+    struct owner {
+    string name;
+    uint id;
+    }
+    function showLand
+
+    function registerLand ()view public{
+        //showland
+    }
+
+}
